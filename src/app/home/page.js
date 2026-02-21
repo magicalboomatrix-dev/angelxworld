178e6718a271a5ac9f38f2574a6b421e9c48423c
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-22 00:07:50 +0530

    Update page.js

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,7 +2,7 @@ import HomePage from "./homeClient.js";
 import { Metadata } from 'next';
 
 export const metadata = {
-  title: "Angelx Official – Best Platform to Buy & Sell USDT | Angelxsss",
+  title: "Angelx Official – Best Platform to Buy & Sell USDT | Angelx",
 
   description:
     "Convert USDT to INR at premium rates with AngelX. Fast processing, instant payouts, strong security, reliable support, and easy crypto selling—your seamless exchange solution.",
