0aa37c1b51e4bd83ded2c96d6a5fe89e28ad8258
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-21 15:18:49 +0530

    Update page.js

--- src/app/invite/page.js
+++ src/app/invite/page.js
@@ -74,7 +74,7 @@ export default function DemoPage() {
       </table>
     </div>
 
-    <div className="login-bx" style={{"margin":"0 0 0 0"}}><button className="login-btn open-btn" onClick={() => setIsOpen(true)}>Invite Friends</button></div>
+    <div className="login-bx" style={{"margin":"0 0 0 0",padding: 0}}><button className="login-btn open-btn" onClick={() => setIsOpen(true)} style={{width:"100%"}}>Invite Friends</button></div>
 
         
   </div>
