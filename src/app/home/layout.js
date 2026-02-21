9d727216f4ce14a3ee021cbf5bb4b48214e359ae
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-21 16:05:51 +0530

    Update layout.js

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -59,6 +59,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
  
-     return children
+     return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
  
 }
