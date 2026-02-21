77ecd7b99544817fca5821af215ab46229533739
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-21 13:09:55 +0530

    Update page.js

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -48,7 +48,7 @@ export default function DemoPage() {
       <main className="content-wrapper">
         <div className="brdc">
           <div className="back-btn-container">
-            <Link href="/home" className="back-link">
+            <Link href="/home" className="back-link" style={{position: 'relative',zIndex: '999'}}>
           <img src="/images/back-btn.png" alt="back" />
         </Link>
           </div>
