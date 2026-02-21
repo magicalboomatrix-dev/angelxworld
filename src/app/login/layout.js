84b3ace2150506f2a5cdab78db9cec311f14fad2
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-21 18:48:35 +0530

    Update layout.js

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "Welcome to AngelXss",
+  title: "Welcome to AngelX",
   /*description:
     "Log in to AngelX for instant USDT to INR conversions at top rates. Fast processing, secure payouts, reliable support—quickly access your account and sell crypto easily.",*/
   keywords: [
